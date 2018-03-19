7274ebc2f226c96fc37dde5d473ae04dfc4c5659
Connor <connor.mckenzie22@gmail.com>
2018-03-19 13:23:09 +1300

    Mobile version of landing page and nav added.

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,8 +1,39 @@
-var lFollowX = 0,
+//Check browser width/
+var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+    lFollowX = 0,
     lFollowY = 0,
     x = 0,
     y = 0,
-    friction = 5 / 30;
+    friction = 5 / 30,
+    namesList = document.getElementsByClassName('names')[0];
+
+
+    function checkWidth(){
+
+     console.log(width);
+     if(width<990){
+       if(namesList.classList.contains('names-desktop')){
+        namesList.classList.remove('names-desktop');
+        namesList.classList.add('names-mobile');
+      }
+    } else if(width>990){
+      console.log(namesList.classList.contains('names-desktop'));
+      if(namesList.classList.contains('names-desktop')===false){
+        namesList.classList.add('names-desktop');
+        namesList.classList.remove('names-mobile')
+      }
+    }
+
+  }
+
+  checkWidth();
+
+//If window resizes change the browser width value
+window.addEventListener('resize', function(event){
+  width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  checkWidth();
+});
+
 
 function moveBackground() {
   x += (lFollowX - x) * friction;
@@ -10,9 +41,7 @@ function moveBackground() {
   
   translate = 'translate(' + x + 'px, ' + y + 'px) scale(1)';
 
-
-
-  $('.names').css({
+  $('.names-desktop').css({
     '-webit-transform': translate,
     '-moz-transform': translate,
     'transform': translate
@@ -28,57 +57,54 @@ $(window).on('mousemove click', function(e) {
   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
   lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
   lFollowY = (10 * lMouseY) / 100;
-
-
-
 });
 
 moveBackground();
 
-$('.names li:nth-child(1) a').mouseover(function() {
-  $('body').addClass('bg-01');
-});
+// $('.names li:nth-child(1) a').mouseover(function() {
+//   $('body').addClass('bg-01');
+// });
 
-$('.names li:nth-child(1) a').mouseout(function() {
-  $('body').removeClass('bg-01');
-});
+// $('.names li:nth-child(1) a').mouseout(function() {
+//   $('body').removeClass('bg-01');
+// });
 
-//-------------------------------------------------//
+// //-------------------------------------------------//
 
-$('.names li:nth-child(2) a').mouseover(function() {
-  $('body').addClass('bg-02');
-});
+// $('.names li:nth-child(2) a').mouseover(function() {
+//   $('body').addClass('bg-02');
+// });
 
-$('.names li:nth-child(2) a').mouseout(function() {
-  $('body').removeClass('bg-02');
-});
+// $('.names li:nth-child(2) a').mouseout(function() {
+//   $('body').removeClass('bg-02');
+// });
 
-//-------------------------------------------------//
+// //-------------------------------------------------//
 
-$('.names li:nth-child(3) a').mouseover(function() {
-  $('body').addClass('bg-03');
-});
+// $('.names li:nth-child(3) a').mouseover(function() {
+//   $('body').addClass('bg-03');
+// });
 
-$('.names li:nth-child(3) a').mouseout(function() {
-  $('body').removeClass('bg-03');
-});
+// $('.names li:nth-child(3) a').mouseout(function() {
+//   $('body').removeClass('bg-03');
+// });
 
-//-------------------------------------------------//
+// //-------------------------------------------------//
 
-$('.names li:nth-child(4) a').mouseover(function() {
-  $('body').addClass('bg-04');
-});
+// $('.names li:nth-child(4) a').mouseover(function() {
+//   $('body').addClass('bg-04');
+// });
 
-$('.names li:nth-child(4) a').mouseout(function() {
-  $('body').removeClass('bg-04');
-});
+// $('.names li:nth-child(4) a').mouseout(function() {
+//   $('body').removeClass('bg-04');
+// });
 
-//-------------------------------------------------//
+// //-------------------------------------------------//
 
-$('.names li:nth-child(5) a').mouseover(function() {
-  $('body').addClass('bg-05');
-});
+// $('.names li:nth-child(5) a').mouseover(function() {
+//   $('body').addClass('bg-05');
+// });
 
-$('.names li:nth-child(5) a').mouseout(function() {
-  $('body').removeClass('bg-05');
-});
+// $('.names li:nth-child(5) a').mouseout(function() {
+//   $('body').removeClass('bg-05');
+// });
