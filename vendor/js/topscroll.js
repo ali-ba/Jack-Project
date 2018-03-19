9c1c6d5715c0fc9d9ccefcd322ee2bf45f1958a6
Jade Tang <jadetag13@gmail.com>
2018-03-20 10:59:38 +1300

    topscroll JS code

---
+++ vendor/js/topscroll.js
@@ -0,0 +1,7 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+document.getElementById("topscrollBtn").addEventListener("click", topFunction);
