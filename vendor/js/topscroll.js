5851f92923615def71bf3da9f9af051fc06aed90
Kitty Wu <wubingkitty@gmail.com>
2018-03-21 11:28:22 +1300

    make a js file to come ture scroll animation and add animation to scrool up

--- vendor/js/topscroll.js
+++
@@ -1,7 +0,0 @@
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-document.getElementById("topscrollBtn").addEventListener("click", topFunction);
